e61b6d83cbd041962d04026cb6f333e4c9fed3d1
dengyuli910904 <912617726@qq.com>
2019-01-16 11:02:26 +0800

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,17 @@
 <html>
 <head>
 	<title>@yield('title')-Sample App</title>
+	<link rel="stylesheet" type="text/css" href="/css/app.css?{{time()}}">
 </head>
 <body>
-	@yield('content')
+	@include('layouts._header')
+	<div class="container projects">
+		<div class="col-md-offset-1 col-md-10">
+			@yield('content')
+			@include('layouts._footer')
+		</div>
+	</div>
+	
+	<script type="text/javascript" src="/js/app.js"></script>
 </body>
 </html>
